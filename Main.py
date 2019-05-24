d0a8c51c352127aa3238c171582094f1ef81fdc5
Rio Gunawan <riogunawan8967uajy@gmail.com>
2019-05-25 02:12:17 +0700

    update

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-from flask import Flask,jsonify,request
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from flasgger import Swagger
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +9,12 @@ import tensorflow as tf
 app = Flask(__name__)
 Swagger(app)
 
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 def predictionData(ImgData,sign):
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line
@@ -52,44 +59,47 @@ def inputTask():
     Ini adalah endpoint untuk menambahkan data task
     ---
     tags:
-        - Rest Controller
-    requestBody:
-      content:
-        multipart/form-data:
-          schema:
-            type: object
-            properties:
-              # 'file' will be the field name in this multipart request
-              file:
-                type: string
-                format: binary
+    - Rest Controller
+    parameters:
+     - in: formData
+       name: upfile
+       type: file
+       description: The file to upload.
     responses:
         200:
             description: Sucess Input
     """
 
-    new_task = request.get_json()
-
-    img = new_task['file']
-
+    # check if the post request has the file part
+    if 'upfile' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['upfile']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        result1 = predictionData(file, 'asl')
+        result2 = predictionData(file, 'bisindo')
+        if result1[0] > result2[0]:
+            predict = result1
+        else:
+            predict = result2
+        return jsonify({'meesaage': predict})
     # imgpath = os.getcwd() + '/dataset/Bisindo/C_Bisindo/'
     # imgname = 'C714.jpg'
     # result1 = predictionData(imgpath + imgname, imgname, 'asl')
     # result2 = predictionData(imgpath + imgname, imgname, 'bisindo')
-    result1 = predictionData(img, 'asl')
-    result2 = predictionData(img, 'bisindo')
-    if result1[0] > result2[0]:
-        predict = result1
-    else:
-        predict = result2
-    return jsonify({'meesaage': predict})
+    # result1 = predictionData(img, 'asl')
+    # result2 = predictionData(img, 'bisindo')
+    # if result1[0] > result2[0]:
+    #     predict = result1
+    # else:
+    #     predict = result2
+    return jsonify({'meesaage': 'error'})
 
-
-
-@app.route('/delete/task/<int:id>', methods=['DELETE'])
-def deleteTask(id):
-    del tasks[id]
-    return jsonify({'meesaage': 'success delete'})
 
 
 
